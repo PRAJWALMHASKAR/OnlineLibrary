81fd2b77bd2ca1636aadd3d7908055c23b513216
prajwal mhaskar <33192878+PRAJWALMHASKAR@users.noreply.github.com>
2024-11-19 01:14:16 +0530

    You can add and issue the books from Library.
    Database is not active yet..

--- Library.java
+++ Library.java
@@ -1,22 +1,45 @@
 import java.util.Scanner;
 
-class OnlineLibrary {
-	String[] arr_availableBooks = new String[10];
-	String[] arr_issueBook = new String[10];
-	void addBook(){};
-	void issueBook(){};
-	void returnBook(){};
-	void showAvailableBooks(){};
+class Library{
+	String[] books;
+	int no_of_books;
+	Library(){
+		this.books= new String[100];
+		this.no_of_books= 0;
+	}
+	void addBook(String book){
+		this.books[no_of_books]= book;
+		no_of_books++;
+		System.out.println(book + " has been added!");
+	}
+	void showAvailableBooks(){
+		System.out.println("Available Books are: ");
+		for(String book : this.books){
+			if(book == null){
+				continue;
+			}
+			System.out.println("*" + book);
+		}
+	}
+	void issueBook(String book){
+		for(int i=0; i<this.books.length; i++){
+			if(this.books[i].equals(book)){
+				System.out.Println("The book has been issued!");
+				this.books[i]= null;
+				return;
+			}
+		}
+		System.out.println("This book does not exist");
+	}
+	void returnBook(String book){
+		addBook(book);
+	}
 }
 
-public class Library extends OnlineLibrary {
-public static void main(String[] args) {
-	OnlineLibrary a = new OnlineLibrary();
+public class OnlineLibrary{
+public static void main(String[] args){
+	Library centralLibrary = new Library();
+	centralLibrary.addBook("Think and Grow Rich!!");
 	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the name of the book you want to issue: ");
-	String str = sc.nextLine();
-	
-	a.arr_issueBook[0] = str;
 }
 }
